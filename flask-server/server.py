eb65b6d524b2629d5d95c3c0d1c98bad41148725
joshprince2 <jshprnc@outlook.com>
2025-10-17 18:11:10 -0400

    fix Dies table syntax and clean up

--- flask-server/server.py
+++ flask-server/server.py
@@ -1,7 +1,6 @@
 from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.orm import DeclarativeBase
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 from sqlalchemy import inspect
 
 class Base(DeclarativeBase):
@@ -9,15 +8,15 @@ class Base(DeclarativeBase):
 
 db = SQLAlchemy(model_class=Base)
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:databateKEy@localhost:5432/toolingInventory'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:databa53KEy@localhost:5432/toolingInventory'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
 
 class Dies(db.Model):
     __tablename__ = "dies"
     tool_number: Mapped[int] = mapped_column(primary_key=True)
-    punch_depth: Mapped[float] = mapped_column
-    material_thickness: Mapped[float] = mapped_column
+    punch_depth: Mapped[float]
+    material_thickness: Mapped[float]
     company: Mapped[str] =  mapped_column(nullable=False)
 
 def model_to_dict(obj): 
@@ -27,7 +26,6 @@ def model_to_dict(obj):
 def get_dies():
     die_query= db.session.scalars(db.select(Dies)).all() 
     dies_dict = [model_to_dict(Die) for Die in die_query]
-    print(dies_dict)
     return jsonify(dies_dict)
 
 if __name__ == "__main__":
