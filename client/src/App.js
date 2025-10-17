78c9e08cf7f6f807cc1df19e8ee7c1d8fd2503fc
joshprince2 <jshprnc@outlook.com>
2025-10-17 17:21:33 -0400

    add sqlalchemy create route that queries dies table in postgres and sends to react

--- client/src/App.js
+++ client/src/App.js
@@ -5,28 +5,20 @@ function App() {
   const [data, setData] = useState([{}])
 
   useEffect(() => {
-      fetch("/members").then (
-        res => res.json()
-      ).then (
-          data => {
-            setData(data)
-            console.log(data)
-          }
-      )
+      fetch("/getDies")
+      .then (res => res.json())
+      .then (data => {
+        setData(data)
+        console.log(data)
+      })
   }, []) //only runs once
 
   return (
     <div>
-
-      {(typeof data.members === 'undefined') ? (
-        <p>Loading...</p>
-      ) : ( 
-        data.members.map((member, i) => (
-          <p key ={i}> {member}</p>
-        ))
-        
-      )}
-      
+      <h1>Die List</h1>
+      <pre>
+        {JSON.stringify(data, null, 2)}
+      </pre>
     </div>
   )
 }
--- flask-server/server.py
+++ flask-server/server.py
@@ -1,12 +1,37 @@
-from flask import Flask
+from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import inspect
 
+class Base(DeclarativeBase):
+  pass
+
+db = SQLAlchemy(model_class=Base)
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:databateKEy@localhost:5432/toolingInventory'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db.init_app(app)
 
-# members api route
-@app.route("/members")
-def members():
-    return {"members":["Member1", "Member2", "Member3"]}
+class Dies(db.Model):
+    __tablename__ = "dies"
+    tool_number: Mapped[int] = mapped_column(primary_key=True)
+    punch_depth: Mapped[float] = mapped_column
+    material_thickness: Mapped[float] = mapped_column
+    company: Mapped[str] =  mapped_column(nullable=False)
+
+def model_to_dict(obj): 
+    return {c.key: getattr(obj, c.key) for c in db.inspect(obj).mapper.column_attrs}
+
+@app.route("/getDies")
+def get_dies():
+    die_query= db.session.scalars(db.select(Dies)).all() 
+    dies_dict = [model_to_dict(Die) for Die in die_query]
+    print(dies_dict)
+    return jsonify(dies_dict)
 
 if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
 
