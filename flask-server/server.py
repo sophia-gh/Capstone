68217a480556cc623562a06b612e06e1ddf6ddcc
joshprince2 <jshprnc@outlook.com>
2025-10-20 13:22:10 -0400

    add tables.py

--- flask-server/server.py
+++ flask-server/server.py
@@ -1,26 +1,12 @@
 from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
-from sqlalchemy import inspect
-
-class Base(DeclarativeBase):
-  pass
+from tables import *
 
 db = SQLAlchemy(model_class=Base)
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:databa53KEy@localhost:5432/toolingInventory'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
-
-class Dies(db.Model):
-    __tablename__ = "dies"
-    tool_number: Mapped[int] = mapped_column(primary_key=True)
-    punch_depth: Mapped[float]
-    material_thickness: Mapped[float]
-    company: Mapped[str] =  mapped_column(nullable=False)
-
-def model_to_dict(obj): 
-    return {c.key: getattr(obj, c.key) for c in db.inspect(obj).mapper.column_attrs}
 
 @app.route("/getDies")
 def get_dies():
