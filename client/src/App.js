e37e1df1a1d0ede6ed416f330c65da776b9a6cd8
sophia-gh <stesta4@kent.edu>
2025-11-10 13:14:49 -0500

    comments

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import MainLayout from './components/layout/MainLayout';
 import Footer from './components/layout/Footer';
 import Dashboard from './components/pages/Dashboard';
 import Tooling from './components/pages/Tooling';
+//import Servicing from './components/pages/Servicing'; 
 import Dies from './components/pages/Dies';
 import Admin from './components/pages/Admin';
 import Login from './components/pages/Login';
@@ -18,6 +19,7 @@ function App() {
           <Route path ="/app/apiCallExample" element = {<APICall />} />
           <Route path ="/app/Dashboard" element = {<Dashboard />} />
           <Route path ="/app/Tooling" element = {<Tooling />} />
+          {/*<Route path ="/app/Servicing" element = {<Servicing />} />*/}
           <Route path ="/app/Tooling/Dies" element = {<Dies />} />
           <Route path ="/app/Admin" element = {<Admin />} />
         </Route>
