c02c33c38b9ccd79e94aad35dc84e6c92ab43839
joshprince2 <jshprnc@outlook.com>
2025-11-24 13:44:23 -0500

    rewrite wsgi.py

--- flask-server/wsgi.py
+++ flask-server/wsgi.py
@@ -1,11 +1,8 @@
 import sys
 import flaskr
-# from flaskr import create_app
-# sys.path.insert(0, './flaskr')
+
 app = flaskr.create_app()
-# from flaskr import create_app
+
 if __name__ == "__main__":
-    # create_app = flaskr.create_app()
-    # create_app.run()
     flaskr.create_app.run()
-# app = create_app()
+
